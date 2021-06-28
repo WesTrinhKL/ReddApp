949862cfa4898f92a8c165272c537397e9036a6c
Sean Collier <sean.collier928@yahoo.com>
2021-06-28 15:32:24 -0700

    login form done

--- routes/users.js
+++ routes/users.js
@@ -77,4 +77,53 @@ router.post('/sign-up', csrfProtection, userValidator, asyncHandler(async (req, 
   };
 
 }))
+
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('login', {
+    title: 'Login',
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const loginValidators = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
+router.post('/login', csrfProtection, loginValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      username,
+      password,
+    } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      const user = await db.User.findOne({ where: { username } });
+      if(user !== null) {
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+        if (passwordMatch) {
+          return res.redirect('/');
+        }
+      }
+      errors.push('Login failed for the provided username and password');
+    } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+    }
+
+    res.render('login', {
+      title: 'Login',
+      username,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  }));
+
+
 module.exports = router;
